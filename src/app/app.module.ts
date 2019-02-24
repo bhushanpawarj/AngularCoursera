872e7e33967810d0bbbebfe65acdb44bc4cf7d53
Bhushan Pawar <bhushanpawar@mail.fresnostate.edu>
2019-02-24 00:24:45 -0800

    Promise Part 2 --delay simulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,8 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { FormsModule } from "@angular/forms"; 
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+
 import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
@@ -65,6 +67,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     FormsModule,
     ReactiveFormsModule
   ],
